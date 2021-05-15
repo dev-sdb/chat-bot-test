9087411fc6b4b9ceb029e81c3a4714e5dd0a1298
dev-sdb <78283306+dev-sdb@users.noreply.github.com>
2021-05-14 21:35:18 -0400

    Add files via upload

---
+++ chatbot.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <vector>
+using namespace std;
+char inputs[0];
+void input();
+int main(){
+printf("\nmessage: ");
+scanf("%s", inputs);
+input();
+
+
+
+
+}
+void input(){
+
+if (!strcmp(inputs, "game")){
+printf("Wanna play a game? yes or no\n");
+printf("reply message: ");
+scanf("%s", inputs);
+if (!strcmp(inputs, "not")){
+printf("\nok we wont play");
+}
+if (!strcmp(inputs, "yes")){
+printf("\nok lets play then\n");
+printf("so the game is you need to guesse a number between 1 and 10 type exit to exit");
+}
+if (!strcmp(inputs, "no")){
+printf("\nok we wont play");
+}
+if (!strcmp(inputs, "nah")){
+}
+if (!strcmp(inputs, "okay")){
+printf("\nok lets play then\n");
+printf("so the game is you need to guesse a number between 1 and 10 type exit to exit");
+}
+if (!strcmp(inputs, "yea")){
+printf("\nok lets play then\n");
+printf("so the game is you need to guesse a number between 1 and 10 type exit to exit");
+}
+}
+
+
+
+if (!strcmp(inputs, "how")){
+if (!strcmp(inputs, "are")){
+if (!strcmp(inputs, "you")){
+}
+}
+printf("\nim good");
+}
+
+
+
+if (!strcmp(inputs, "hello")){
+
+
+
+
+
+printf("\nhello how are you!\n");
+printf("reply message: ");
+scanf("%s", inputs);
+if (!strcmp(inputs, "good")){
+printf("thats good");
+}
+if (!strcmp(inputs, "bad")){
+printf("thats not good");
+}
+if (!strcmp(inputs, "ok")){
+printf("okay");
+}
+if (!strcmp(inputs, "okay")){
+printf("okay");
+}
+if (!strcmp(inputs, "hello")){
+printf("\nyou already said hello?\n");
+}
+
+
+
+
+}
+
+main();
+
+}
+
+
+
